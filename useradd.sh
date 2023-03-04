dab0419b2289d378a30c198c70126c93b4a1c833
Cloud User <matthewobono@yahoo.com>
2023-03-04 15:24:36 +0000

    This is a User Management script

---
+++ useradd.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script will add new users
+
+echo Please enter names of new user
+read names
+
+sudo useradd $names
+
+echo Please assign Password to $names
+
+sudo passwd $names
+
+echo Please asisgn $names to an existing group
+
+echo Enter group name
+read group
+
+sudo usermod -G $group $names
+
+echo $names has been added to the $group  group
+sleep 6
+
+echo You are all set. Congratulations!
+
+sleep 10
+
+sudo tail -3 /etc/passwd
+sudo tail -3 /etc/shadow
+sudo tail -10 /etc/group
